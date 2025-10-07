c6e75fcea48abee6ecc1d0bc8ccb74127b2db71e
admin <admin@gmail.com>
2025-10-07 09:54:32 +0100

    Updated Main Class

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 
+using System;
+using UtilityKnife.Converters;
 namespace UtilityKnife
 {
     public static class Program
     {
         static void Main(string[] args)
         {
-            // skeleton program
+            var converter = new FahrenheitToCelsius();
+            var celsius = converter.FahrenheitToCelsiusConverter(212.0);
+            Console.WriteLine($"Fahrenheit temp of 212 is {celsius}.");
         }
     }
 }
