769d319e88a5d114eef9b43dc9c176837ffd6d6c
admin <admin@gmail.com>
2025-10-07 09:52:42 +0100

    Updated Fahrenheit and Celsius Class

--- Feature.cs
+++ Feature.cs
@@ -4,10 +4,13 @@ namespace UtilityKnife.Converters
 {
     public class FahrenheitToCelsius
     {
-        public double FahrenheitToCelsiusConverter(double FahrenheitTemp)
+        public double FahrenheitToCelsiusConverter(double fahrenheitTemp)
         {
-            double _fahreneithTemp = 0.0;
             double _celsius = 0.0;
+            // (F – 32) * 5/9
+            var step1 = Calculator.Calculator.Subtract(fahrenheitTemp, 32);
+            var step2 = Calculator.Calculator.Multiplication(step1, 5.0);
+            _celsius = Calculator.Calculator.Division(step2, 9.0);
             return _celsius;
         }
     }
